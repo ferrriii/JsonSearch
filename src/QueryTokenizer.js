cb322ec95e51702ba42f78334f149865eb655d60
ferrriii <57658129+ferrriii@users.noreply.github.com>
2021-11-17 11:05:13 +0330

    add comments

--- src/QueryTokenizer.js
+++ src/QueryTokenizer.js
@@ -1,6 +1,12 @@
 export default function tokenizeQuery (query, keys) {
   const tokens = []
-  // inspired by https://stackoverflow.com/a/366532/3559463
+  /* inspired by https://stackoverflow.com/a/366532/3559463
+   * regex named groups:
+   *   n: captures negate (symbol - exists or not)
+   *   q: captures actual query (exlcuding ' or ")
+   *        capture groups 4 and 5 also capture the query
+   *   k: captures searched key (in queries like key:something)
+   */
   const searchTokenRegex = new RegExp(`(?<n>-)?(?:(?<k>${keys.join('|')}):)?(?<q>"([^"]*)"|'([^']*)'|[^\\s"']+)`, 'ig')
 
   let token
